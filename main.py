5fa5b38f19eeddd91971df4602668a6470fb1edc
vitekputna <vitekputna@gmail.com>
2023-09-06 13:53:12 +0200

    UAAA

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 from pipesim import *
 
-# comp1 = component(0,1)
-# comp2 = component(1,2)
-# comp3 = component(2,3)
-# comp4 = component(1,3)
-# comp5 = component(4,3)
-# comp6 = component(4,0)
+sim = pipesim()
 
-# sim = topology()
+comp1 = pipe(0,1)
+comp1.length = 1
+comp1.diameter = 1e-2
+comp2 = pipe(1,2)
+comp3 = pipe(2,3)
+comp4 = pipe(3,4)
 
-# sim.add_component(comp1)
-# sim.add_component(comp2)
-# sim.add_component(comp3)
-# sim.add_component(comp4)
-# sim.add_component(comp5)
-# sim.add_component(comp6)
+sim.add_component(comp1)
+sim.add_component(comp2)
+sim.add_component(comp3)
+sim.add_component(comp4)
 
-# sim.print()
-
-sim = pipesim([1,1],[1,3])
 sim.set_solver(pressure_correction_solver)
+
+sim.add_boundary_condition(set_pressure(0,100))
+sim.add_boundary_condition(set_pressure(4,50))
+
 sim.solve()
 
 sim.print_variables()
--- pipesim.py
+++ pipesim.py
@@ -20,9 +20,6 @@ class component:
 
     type = "base"
 
-    def __init__(self) -> None:
-        pass
-
     def __init__(self,inlet_node : int, outlet_node : int) -> None:
         self.inlet_node = inlet_node
         self.outlet_node = outlet_node
@@ -34,23 +31,33 @@ class component:
 class pipe(component):
     type = "pipe"
 
+    def __init__(self, inlet_node: int, outlet_node: int) -> None:
+        self.length = 0
+        self.diameter = 0
+        super().__init__(inlet_node,outlet_node)
+
 class topology:
 
     def __init__(self) -> None:
         self.components = []
         self.nodes = []
 
+        self.N_nodes = 0
+        self.N_components = 0
+
     def add_component(self, component : component) -> None:
+        self.N_components += 1
         self.components.append(component)
         self.add_node_from_component(component)
 
-    def add_node_from_component(self, component : component) -> None:
-        
+    def add_node_from_component(self, component : component) -> None:   
         num_nodes_toadd = max(0,component.inlet_node-len(self.nodes)+1)
         num_nodes_toadd = max(num_nodes_toadd,component.outlet_node-len(self.nodes)+1)
 
         for i in range(num_nodes_toadd):
             self.nodes.append(node())
+
+        self.N_nodes = len(self.nodes)
 
         component_ID = len(self.components)-1
 
@@ -61,7 +68,6 @@ class topology:
         return self.components[component_id]
 
     def print(self) -> None:
-
         print("Components:")
         for component in self.components:
             print(component.type, component.inlet_node, component.outlet_node)
@@ -73,46 +79,83 @@ class topology:
 class variables:
 
     def __init__(self) -> None:
-        pass
+        self.values_per_node = 0
+        self.values_per_component = 0
 
-    def __init__(self, per_node : int, per_component : int) -> None:
-        self.values_per_node = per_node
-        self.values_per_component = per_component
-
-    def init_variables(self, per_node_size : int, per_component_size : int) -> None:
-        self.node_values = np.zeros((self.values_per_node,per_node_size))
-        self.component_values = np.zeros((self.values_per_component,per_component_size))
-
+    def init_variables(self, N_nodes : int, N_components : int) -> None:
+        self.node_values = np.zeros((self.values_per_node,N_nodes))
+        self.component_values = np.zeros((self.values_per_component,N_components))
 
 class solver:
 
     def __init__(self) -> None:
         pass
 
-    def solve(self, variables : variables):
-        print("")
+    def solve(self, variables : variables, topology : topology, boundary_condition : list) -> bool:
+        return True
+    
+    def get_nodes(self, topology : topology, boundary_condition : list) -> list:
+        nodes_to_solve = []
+        found = False
+        for i in range(topology.N_nodes):
+            for boundary in boundary_condition:
+                if boundary.index == i:
+                    found = True
+
+            if found == True:
+                found = False
+                continue
+
+            nodes_to_solve.append(i)
+
+        return nodes_to_solve
+
 
 class pressure_correction_solver(solver):
 
-    def solve(self, variables : variables):
+    def solve(self, variables : variables, topology : topology, boundary_condition : list) -> bool:
         print("Incompressible stacionary solver")
-        size = len(variables.node_values)
-        print(size)
-        A = np.zeros((size,size))
-        
+        size = len(variables.node_values[0])
 
+        nodes_to_solve = self.get_nodes(topology,boundary_condition)
+
+        print(nodes_to_solve)
+            
+
+        # A = np.zeros((size,size))
+
+class condition:
+    def __init__(self, index : int) -> None:
+        self.index = index
+
+class set_pressure(condition):
+    def __init__(self, node_idx: int, pressure : float) -> None:
+        type = "node"
+        self.pressure = pressure
+        super().__init__(node_idx)
+        
 class pipesim:
 
-    def __init__(self, node_variable_size : list, component_variable_size :list) -> None:
+    def __init__(self) -> None:
         self.topology = topology()
-        self.variables = variables(node_variable_size[0],component_variable_size[0])
-        self.variables.init_variables(node_variable_size[1],component_variable_size[1])
+        self.variables = variables()
+        self.boundary_conditions = []
 
     def set_solver(self, solver : solver) -> None:
         self.solver = solver()
 
+    def add_component(self, component : component) -> None:
+        self.topology.add_component(component)
+
+        self.variables.values_per_component = 1
+        self.variables.values_per_node = 1
+        self.variables.init_variables(self.topology.N_nodes,self.topology.N_components)
+
+    def add_boundary_condition(self,boundary_condition : condition) -> None:
+        self.boundary_conditions.append(boundary_condition)
+
     def solve(self) -> None:
-        self.solver.solve(self.variables)
+        self.solver.solve(self.variables,self.topology,self.boundary_conditions)
 
     def print_variables(self) -> None:
         print(self.variables.node_values)
